54eb5b2f8b2759b62add329471a9defd306d3a9e
Raisul Karim Saju <rksaju80@gmail.com>
2024-09-07 23:46:15 +0600

    animation added in title

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -1,43 +1,76 @@
 import React from "react";
+import { motion } from "framer-motion";
+
+// Define animation variants
+const socialIconVariants = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  hover: { scale: 1.2 },
+  tap: { scale: 0.9 },
+};
 
 const Social = () => {
   return (
     <div className="home__social">
-      <a
+      <motion.a
         href="https://www.linkedin.com/in/raisul-karim-saju/"
         className="home__social-icon"
         target="_blank"
         rel="noreferrer"
+        variants={socialIconVariants}
+        initial="initial"
+        animate="animate"
+        whileHover="hover"
+        whileTap="tap"
+        transition={{ duration: 0.5 }}
       >
-        <i class="bx bxl-linkedin"></i>
-      </a>
+        <i className="bx bxl-linkedin"></i>
+      </motion.a>
 
-      <a
+      <motion.a
         href="https://github.com/RK289g"
         className="home__social-icon"
         target="_blank"
         rel="noreferrer"
+        variants={socialIconVariants}
+        initial="initial"
+        animate="animate"
+        whileHover="hover"
+        whileTap="tap"
+        transition={{ duration: 0.5, delay: 0.2 }}
       >
-        <i class="uil uil-github"></i>
-      </a>
+        <i className="uil uil-github"></i>
+      </motion.a>
 
-      <a
+      <motion.a
         href="https://www.instagram.com/raisul_saju?igsh=MWRyY2VieGlkdWI2Nw=="
         className="home__social-icon"
         target="_blank"
         rel="noreferrer"
+        variants={socialIconVariants}
+        initial="initial"
+        animate="animate"
+        whileHover="hover"
+        whileTap="tap"
+        transition={{ duration: 0.5, delay: 0.4 }}
       >
-        <i class="uil uil-instagram"></i>
-      </a>
+        <i className="uil uil-instagram"></i>
+      </motion.a>
 
-      <a
+      <motion.a
         href="https://twitter.com/raisul_karim1"
         className="home__social-icon"
         target="_blank"
         rel="noreferrer"
+        variants={socialIconVariants}
+        initial="initial"
+        animate="animate"
+        whileHover="hover"
+        whileTap="tap"
+        transition={{ duration: 0.5, delay: 0.6 }}
       >
-        <i class="uil uil-twitter"></i>
-      </a>
+        <i className="uil uil-twitter"></i>
+      </motion.a>
     </div>
   );
 };
