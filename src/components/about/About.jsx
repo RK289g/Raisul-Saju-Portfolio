05014e74fcd70b335a4095697d03d3a77e701c94
Raisul Karim Saju <rksaju80@gmail.com>
2024-10-02 23:25:12 +0600

    image resize

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -57,9 +57,9 @@ const About = () => {
             <svg
               class="button__icon"
               xmlns="http://www.w3.org/2000/svg"
-              width="26"
-              height="26"
-              viewBox="0 0 26 26"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
               fill="none"
             >
               <path
